0d95e845ca4c24e1a0effe681ac9b7f325a41d29
Jeffrey <exdiark@gmail.com>
2023-11-20 21:54:43 -0800

    new test program

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,73 @@
-#include <wx/wx.h>
 
-class App : public wxApp {
-public:
-    bool OnInit() {
-        wxFrame* window = new wxFrame(NULL, wxID_ANY, "GUI Test", wxDefaultPosition, wxSize(600, 400));
-        wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
-        wxStaticText* text = new wxStaticText(window, wxID_ANY, "Well Done!\nEverything seems to be working",
-            wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
-        text->SetFont(wxFont(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
-        sizer->Add(text, 1, wxALIGN_CENTER);
-        window->SetSizer(sizer);
-        window->Show();
-        return true;
-    }
+#include <../Header.h>
+
+class MyApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
 };
 
-wxIMPLEMENT_APP(App);
+class MyFrame : public wxFrame
+{
+    public:
+        MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+
+    private:
+        void OnHello(wxCommandEvent& event);
+        void OnExit(wxCommandEvent& event);
+        void OnAbout(wxCommandEvent& event);
+        wxDECLARE_EVENT_TABLE();
+};
+
+enum
+{
+    ID_Hello = 1
+};
+
+wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+wxEventTableEntry(wxEVT_MENU, ID_Hello, wxID_ANY, wxNewEventTableFunctor(wxEVT_MENU, wxCommandEventHandler(MyFrame::OnHello)), 0),
+EVT_MENU(wxID_EXIT, MyFrame::OnExit)
+EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+wxEND_EVENT_TABLE()
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit()
+{
+    MyFrame* frame = new MyFrame("Hello World", wxPoint(50, 50), wxSize(450, 340));
+    frame->Show(true);
+    return true;
+}
+
+MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+    : wxFrame(NULL, wxID_ANY, title, pos, size)
+    {
+        wxMenu* menuFile = new wxMenu;
+        menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+            "Help string shown in status bar for this menu item");
+        menuFile->AppendSeparator();
+        menuFile->Append(wxID_EXIT);
+        wxMenu* menuHelp = new wxMenu;
+        menuHelp->Append(wxID_ABOUT);
+        wxMenuBar* menuBar = new wxMenuBar;
+        menuBar->Append(menuFile, "&File");
+        menuBar->Append(menuHelp, "&Help");
+        SetMenuBar(menuBar);
+        CreateStatusBar();
+        wxStatusBarBase::SetStatusText("Welcome to wxWidgets!");
+    }
+    void MyFrame::OnExit(wxCommandEvent& event)
+    {
+        Close(true);
+    }
+
+    void MyFrame::OnAbout(wxCommandEvent& event)
+    {
+        wxMessageBox("This is a wxWidgets' Hello world sample",
+            "About Hello World", wxOK | wxICON_INFORMATION);
+    }
+
+    void MyFrame::OnHello(wxCommandEvent& event)
+    {
+        wxLogMessage("Hello world from wxWidgets!");
+    }
+}
